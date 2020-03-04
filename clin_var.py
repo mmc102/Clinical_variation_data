0f5f764efe8a971db8a79a058a59a94684fbc361
Matthew Carroll <60335800+mmc102@users.noreply.github.com>
2020-03-03 22:37:25 -0800

    updated to add data processing and crude random forst model

--- clin_var.py
+++ clin_var.py
@@ -1,7 +1,8 @@
 import os 
 import pandas as pd 
 from sklearn.model_selection import train_test_split
-
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_squared_error
 data = pd.read_csv('/Users/mattcarroll/Downloads/clinvar_conflicting.csv')
 
 
@@ -28,9 +29,42 @@ xTest_numerical = xTest.select_dtypes(exclude = ['object'])
 
 cols_missing_values = ['ORIGIN', 'SSR','DISTANCE','STRAND','STRAND','MOTIF_POS','MOTIF_SCORE_CHANGE','LoFtool','CADD_PHRED','CADD_RAW','BLOSUM62']
 
+#should plot these to make sure none of the values are outliers that are skewing the mean 
+#not best practice to assume that the mean is the best bet, but i will come back to this 
+
 for i in cols_missing_values:
     xTrain_numerical[i].fillna(xTrain[i].mean(),inplace = True)
     xTest_numerical[i].fillna(xTest[i].mean(),inplace = True)
 
-    #still unclear to me if i want to impute values for my test subset of data
-    
+#dropping these columns as they are mostly null. should investigate the data and potentially OHE if few possible states
+xTest_numerical = xTest_numerical.drop(['MOTIF_POS','MOTIF_SCORE_CHANGE'], axis =1)
+xTrain_numerical = xTrain_numerical.drop(['MOTIF_POS','MOTIF_SCORE_CHANGE'], axis =1)
+
+#still unclear to me if i want to impute values for my test subset of data
+#i think the answer is yes based on some cursury reading 
+
+#n_jobs = -1 for use of all four cores 
+rnd_forest_rggr = RandomForestRegressor(n_estimators=500, verbose = False, n_jobs=-1)
+
+rnd_forest_rggr.fit(xTrain_numerical,yTrain)
+
+print(rnd_forest_rggr.feature_importances_)
+#forecast for y values 
+y_pred = rnd_forest_rggr.predict(xTest_numerical)
+
+#prepare the MSE for the test data 
+
+mse = mean_squared_error(yTest,y_pred)
+
+
+#needs work, determine % accuracy of predictions 
+y_pred = y_pred.tolist()
+yTest = yTest.tolist()
+correct = 0
+for i in range(len(yTest)):
+    if yTest[i] == y_pred[i]:
+        correct +=1 
+
+print(correct/len(yTest))
+
+print(mse)
